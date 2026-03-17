504ee3e5f302e9bdb4c02e987d7b471fe38db230
gumba <gumba6740@gmail.com>
2026-03-17 18:00:25 +0900

    Chapter 02 (#2)
    * feat: chapter_02 과제 업로드
    
    * feat: chapter_02 과제 업로드

---
+++ To_Do_List/app/to_do_list/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from app.to_do_list.models import ToDoList
+
+@admin.register(ToDoList)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'is_completed', 'start_date', 'end_date']
+    list_display_links = ['title', 'description', 'is_completed', 'start_date', 'end_date']
+    list_filter = ['start_date', 'end_date', 'is_completed']
---
+++ To_Do_List/app/to_do_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ToDoListConfig(AppConfig):
+    name = 'app.to_do_list'
