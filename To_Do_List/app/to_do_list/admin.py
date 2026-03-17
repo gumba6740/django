21e25f10d5a914fdee88f3dd486424273f750fab
gumba <gumba6740@gmail.com>
2026-03-17 17:51:55 +0900

    feat: chapter_02 과제 업로드

--- To_Do_List/app/to_do_list/admin.py
+++ To_Do_List/app/to_do_list/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from app.to_do_list.models import ToDoList
+
+@admin.register(ToDoList)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'description', 'is_completed', 'start_date', 'end_date']
+    list_display_links = ['title', 'description', 'is_completed', 'start_date', 'end_date']
+    list_filter = ['start_date', 'end_date', 'is_completed']
