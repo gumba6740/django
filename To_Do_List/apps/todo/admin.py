f892d968cfad8cde78d578269c13bbd66eec760c
gumba <gumba6740@gmail.com>
2026-03-23 21:48:04 +0900

    댓글 목록 및 pagination 추가

--- To_Do_List/apps/todo/admin.py
+++ To_Do_List/apps/todo/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from apps.todo.models import ToDoList
+from apps.todo.models import ToDoList, Comment
+
+
+admin.site.register(Comment)
+class CommentInline(admin.TabularInline):
+    model = Comment
+    fields = ['message', 'user']
+
 
 @admin.register(ToDoList)
 class TodoAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'description', 'is_completed', 'start_date', 'end_date']
     list_display_links = ['title', 'description', 'is_completed', 'start_date', 'end_date']
     list_filter = ['start_date', 'end_date', 'is_completed']
+    inlines = [
+        CommentInline,
+    ]
+
--- To_Do_List/apps/todo/cbvs.py
+++ To_Do_List/apps/todo/cbvs.py
@@ -1,8 +1,11 @@
+from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from apps.todo.models import ToDoList
+
+from apps.todo.forms import CommentForm
+from apps.todo.models import ToDoList, Comment
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -15,11 +18,13 @@ class ToDoListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
 
+# 할일과 작성자 매칭
         queryset = super().get_queryset()
-
+    # 어드민 권한
         if not self.request.user.is_superuser:
             queryset = queryset.filter(author=self.request.user)
 
+# 검색 기능
         q = self.request.GET.get('q')
         if q:
             queryset = queryset.filter(
@@ -35,9 +40,10 @@ class TodoDetailView(LoginRequiredMixin, DetailView):
     model = ToDoList
     template_name = 'todo/cbv_todo_info.html'
 
+# 할일과 작성자 매칭
     def get_object(self, queryset=None):
         object = super().get_object()
-
+    # 어드민 권한
         if self.request.user.is_superuser:
             return object
 
@@ -45,10 +51,19 @@ class TodoDetailView(LoginRequiredMixin, DetailView):
             raise Http404()
         return object
 
+# context에 todo, comment_form 추가
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['todo'] = self.object.__dict__
-        print(context['todo'])
+        context['comment_form'] = CommentForm()
+
+# 댓글 페이지네이터
+        comments = Comment.objects.filter(todo_id=self.object.id)
+        paginator = Paginator(comments, 10)
+        page = self.request.GET.get('page')
+        page_obj = paginator.get_page(page)
+        context['page_obj'] = page_obj
+
         return context
 
 
@@ -57,6 +72,7 @@ class TodoCreateView(LoginRequiredMixin, CreateView):
     template_name = 'todo/cbv_create_todo.html'
     fields = ['title', 'description', 'start_date', 'end_date']
 
+# form을 받아서 작성자 추가 및 디비에 저장
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.author = self.request.user
@@ -68,16 +84,16 @@ class TodoCreateView(LoginRequiredMixin, CreateView):
         return reverse_lazy('cbv_todo:info', kwargs={'pk': self.object.pk})
 
 
-
 class TodoUpdateView(LoginRequiredMixin, UpdateView):
     model = ToDoList
     template_name = 'todo/cbv_update_todo.html'
     fields = ['title', 'description', 'is_completed', 'start_date', 'end_date']
     context_object_name = 'todo'
 
+# 할일과 작성자 매칭
     def get_object(self, queryset=None):
         object = super().get_object()
-
+    # 어드민 권한
         if self.request.user.is_superuser:
             return object
 
@@ -89,13 +105,13 @@ class TodoUpdateView(LoginRequiredMixin, UpdateView):
         return reverse_lazy('cbv_todo:info', args=(self.object.pk,))
 
 
-
 class TodoDeleteView(LoginRequiredMixin, DeleteView):
     model = ToDoList
 
+# 할일과 작성자 매칭
     def get_object(self, queryset=None):
         object = super().get_object()
-
+    # 어드민 권한
         if self.request.user.is_superuser:
             return object
 
