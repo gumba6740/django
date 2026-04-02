1b806d30c257f3245fc422f58618fe4c1109e6bd
gumba <gumba6740@gmail.com>
2026-04-02 14:35:07 +0900

    test: model test

--- Goodeat/restaurants/tests.py
+++ Goodeat/restaurants/tests.py
@@ -1,3 +1,29 @@
 from django.test import TestCase
+from restaurants.models import Restaurant
 
-# Create your tests here.
+
+class RestaurantModelTest(TestCase):
+    def setUp(self):
+        self.restaurant_info = {
+            "name": "Test Restaurant",
+            "description": "Test Description",
+            "address":  "Test Address",
+            "contact": "Test Contact",
+            "open_time": "10:00:00",
+            "close_time": "22:00:00",
+            "last_order": "21:00:00",
+            "regular_holiday": "MON"
+        }
+    def test_create_restaurant(self):
+        restaurant = Restaurant.objects.create(**self.restaurant_info)
+
+        self.assertEqual(Restaurant.objects.count(), 1)
+        self.assertEqual(restaurant.name, self.restaurant_info['name'])
+        self.assertEqual(restaurant.description, self.restaurant_info['description'])
+        self.assertEqual(restaurant.address, self.restaurant_info['address'])
+        self.assertEqual(restaurant.contact, self.restaurant_info['contact'])
+        self.assertEqual(restaurant.open_time, self.restaurant_info['open_time'])
+        self.assertEqual(restaurant.close_time, self.restaurant_info['close_time'])
+        self.assertEqual(restaurant.last_order, self.restaurant_info['last_order'])
+        self.assertEqual(restaurant.regular_holiday, self.restaurant_info['regular_holiday'])
+        self.assertEqual(str(restaurant), self.restaurant_info['name'])
